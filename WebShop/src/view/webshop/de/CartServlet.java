65a60581b7e3982a3a3c06c155e1268efb911313
philenius <philippus-perez@web.de>
2016-04-09 02:30:47 +0200

    added post method for adding cakes to shopping cart

--- WebShop/src/view/webshop/de/CartServlet.java
+++ WebShop/src/view/webshop/de/CartServlet.java
@@ -7,10 +7,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
 public class CartServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8721110402254298502L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect("cart.jsp");
+		resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		String jsonData = HelperClass.ReadJSONPostData(req);
+
+		JSONObject object = new JSONObject(jsonData);
+		int cake_id = object.getInt("cake_id");
+		
+		// TODO: Find cake and save in shopping cart
+
+		resp.setStatus(HttpServletResponse.SC_CREATED);
+
 	}
 }

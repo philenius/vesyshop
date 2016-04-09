cdbdae97a2efbf96c693db70497b309fb7549ab0
philenius <philippus-perez@web.de>
2016-04-09 02:41:13 +0200

    added response for POST method

--- WebShop/src/view/webshop/de/AllCakes.java
+++ WebShop/src/view/webshop/de/AllCakes.java
@@ -1,9 +1,13 @@
 package view.webshop.de;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import mocking.Cake;
 
@@ -38,4 +42,9 @@ public class AllCakes extends HttpServlet {
 		out.write(responseJSON);
 		out.close();
 	};
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+	}
 }

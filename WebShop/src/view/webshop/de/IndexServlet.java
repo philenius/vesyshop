e91f43b04a676a34183d884ecf4aa1903e177061
philenius <philippus-perez@web.de>
2016-04-12 01:06:23 +0200

    added exception handling and check for parameters

--- WebShop/src/view/webshop/de/IndexServlet.java
+++ WebShop/src/view/webshop/de/IndexServlet.java
@@ -1,6 +1,11 @@
 package view.webshop.de;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class IndexServlet extends HttpServlet {
 
@@ -9,10 +14,21 @@ public class IndexServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1867533624040259923L;
 
+	/**
+	 * Returns the actual Single Page Application (SPA).
+	 */
 	@Override
 	protected void doGet(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse resp)
 			throws javax.servlet.ServletException, java.io.IOException {
-		
-		resp.sendRedirect("index.jsp");
+
+		// Creates session if there is none
+		req.getSession();
+
+		resp.sendRedirect("resources/index.html");
 	};
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+	}
 }
--- WebShop/src/view/webshop/de/RegisterServlet.java
+++ WebShop/src/view/webshop/de/RegisterServlet.java
@@ -7,17 +7,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RegisterServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6808035424949277851L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.sendRedirect("register.jsp");
+		resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 	}
 
+	/**
+	 * Handles registration of a new user.
+	 */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String username = req.getParameter("username");
-		String password = req.getParameter("password");
+		String jsonData = HelperClass.ReadJSONPostData(req);
+		
+		try {
+			JSONObject jsonObject = new JSONObject(jsonData);
+			String user = jsonObject.getString("user");
+			String password = jsonObject.getString("password");
 
-		resp.sendRedirect("index");
+			// User or password missing
+			if ((user == null) || (password == null)) {
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+				return;
+			}
+			
+			// TODO: Save user in DB
+			
+			resp.setStatus(HttpServletResponse.SC_OK);
+			return;
+		}
+		catch(JSONException ex) {
+			System.out.println(ex);
+		}
+		catch(Exception ex) {
+			System.out.println(ex);
+		}
+		resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 	}
 }

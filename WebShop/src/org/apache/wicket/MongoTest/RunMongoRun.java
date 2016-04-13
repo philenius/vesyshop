5507654e9bdb75ea14ae902f449c10ac1cb5a074
philenius <philippus-perez@web.de>
2016-04-13 23:31:03 +0200

    added new cakes, recipes and ingredients

--- WebShop/src/org/apache/wicket/MongoTest/RunMongoRun.java
+++ WebShop/src/org/apache/wicket/MongoTest/RunMongoRun.java
@@ -26,32 +26,52 @@ public class RunMongoRun {
 		
 		Connector con = new Connector();
 		db = con.getDatabase();
-		/*
+		
 		Manager.deleteAllDocsIn(db, DbNames.collection.CAKES.toString());
 		Manager.deleteAllDocsIn(db, DbNames.collection.CARTS.toString());
+		/*
 		Manager.deleteAllDocsIn(db, DbNames.collection.INGRIDIENTS.toString());
 		Manager.deleteAllDocsIn(db, DbNames.collection.ORDERS.toString());
 		Manager.deleteAllDocsIn(db, DbNames.collection.RECEPTS.toString());
 		Manager.deleteAllDocsIn(db, DbNames.collection.USERS.toString());
-		
-		Ingridient zucker 			= new Ingridient("Zucker", 12, 13, DbNames.QuantityType.gram.toString());
-		Recept ZitronenkuchenRezept = new Recept(20, 200, "Rezept Zitronenkuchen", asList(zucker));
-		Cake ZitronenKuchen 		= new Cake(ZitronenkuchenRezept, "Zitronenkuchen");
-		
-		Recept ApfelkuchenRezept 	= new Recept(20, 200, "Rezept Apfelkuchen", asList(zucker));
-		Cake Apfelkuchen 			= new Cake(ApfelkuchenRezept, "Apfelkuchen");
-				
-		zucker.save(db);
-		ZitronenkuchenRezept.save(db);
-		ZitronenKuchen.save(db);
-		Apfelkuchen.save(db);
-		ApfelkuchenRezept.save(db);
-		
-		Ingridient.getAll(db);
-		Recept.getAll(db);
-		ShoppingCart.getAll(db);
-		User.getAll(db);
 		*/
+		Ingridient zucker 			= new Ingridient("Zucker", 12, 13, DbNames.QuantityType.gram.toString());
+		zucker.save(db);
+		Ingridient zitronen 		= new Ingridient("Zitronen", 8, 100, DbNames.QuantityType.gram.toString());
+		zitronen.save(db);
+		Ingridient mehl 			= new Ingridient("Mehl", 1, 600, DbNames.QuantityType.gram.toString());
+		mehl.save(db);
+		Ingridient alcohol			= new Ingridient("Schnaps", 17, 200, DbNames.QuantityType.ml.toString());
+		alcohol.save(db);
+		Ingridient kaese			= new Ingridient("Kaese", 4, 450, DbNames.QuantityType.gram.toString());
+		kaese.save(db);
+		Ingridient apfel			= new Ingridient("Aepfel", 3, 700, DbNames.QuantityType.gram.toString());
+		apfel.save(db);
+		
+		Recept ZitronenkuchenRezept = new Recept(20, 200, "Rezept für Zitronenkuchen", asList(zucker, zitronen, mehl));
+		ZitronenkuchenRezept.save(db);
+		Cake ZitronenKuchen 		= new Cake(ZitronenkuchenRezept, "Zitronenkuchen");
+		ZitronenKuchen.save(db);
+		
+		Recept ApfelkuchenRezept 	= new Recept(20, 200, "Rezept für Apfelkuchen", asList(apfel, zucker, mehl));
+		ApfelkuchenRezept.save(db);
+		Cake Apfelkuchen 			= new Cake(ApfelkuchenRezept, "Apfelkuchen");
+		Apfelkuchen.save(db);
+		
+		Recept SchwarzwaelderRezept = new Recept(27, 200, "Rezept für Schwarzwaelder Kirschtorte", asList(alcohol, zucker, mehl));
+		SchwarzwaelderRezept.save(db);
+		Cake Schwarzwaelder 		= new Cake(SchwarzwaelderRezept, "Schwarzwaelder Kirschtorte");
+		Schwarzwaelder.save(db);
+		
+		Recept KaeseKuchenRezept 	= new Recept(15, 120, "Rezept für Kaesekuchen", asList(kaese, zucker, mehl));
+		KaeseKuchenRezept.save(db);
+		Cake KaeseKuchen			= new Cake(KaeseKuchenRezept, "Kaesekuchen");
+		KaeseKuchen.save(db);		
+		
+//		Ingridient.getAll(db);
+//		Recept.getAll(db);
+//		ShoppingCart.getAll(db);
+//		User.getAll(db);
 	}
 
 }
